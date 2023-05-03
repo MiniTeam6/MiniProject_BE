343e9a730e3050690878423f3b5d2e94ba4aade7
yeeeeerim <gyr3904@gmail.com>
2023-05-04 00:13:09 +0900

    push하기 전

--- src/main/java/shop/mtcoding/restend/model/Event/EventRepository.java
+++ src/main/java/shop/mtcoding/restend/model/Event/EventRepository.java
@@ -2,9 +2,15 @@ package shop.mtcoding.restend.model.Event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+<<<<<<< HEAD
 import java.util.List;
 
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAll();
+=======
+public interface EventRepository extends JpaRepository<Event,Long> {
+
+
+>>>>>>> a7036f2 (one to one 관계 추가, service 회원전체리스트, 검색 기능 추가)
 }
