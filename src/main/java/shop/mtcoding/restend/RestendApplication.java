8f3c5524a79a7a92526e53ba33ca0a6ee2dd08a9
ji-hoooon <ljh6384@naver.com>
2023-05-16 00:17:36 +0900

    sentry 재설정

--- src/main/java/shop/mtcoding/restend/RestendApplication.java
+++ src/main/java/shop/mtcoding/restend/RestendApplication.java
@@ -1,9 +1,11 @@
 package shop.mtcoding.restend;
 
+import io.sentry.spring.EnableSentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableSentry
 public class RestendApplication {
     public static void main(String[] args) {
         // Sentry.io 연결해서 !!
