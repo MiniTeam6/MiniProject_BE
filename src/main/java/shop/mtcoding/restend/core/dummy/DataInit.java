5763718a1f50a6ed439d7248efd76a01ae2f5400
ji-hoooon <ljh6384@naver.com>
2023-05-15 20:58:22 +0900

    사용자 중복 등록 방지

--- src/main/java/shop/mtcoding/restend/core/dummy/DataInit.java
+++ src/main/java/shop/mtcoding/restend/core/dummy/DataInit.java
@@ -30,42 +30,32 @@ public class DataInit extends DummyEntity {
     @Bean
     CommandLineRunner initDev(UserRepository userRepository, EventRepository eventRepository, AnnualRepository annualRepository, DutyRepository dutyRepository, OrderRepository orderRepository) {
         return args -> {
-            User ssar = null;
-            User cos = null;
-            User love = null;
-            if (userRepository.findByUsername("사르").isEmpty()) {
-                ssar = userRepository.save(newUser("사르", "ADMIN", true));
-            } else {
-                ssar = userRepository.findByUsername("사르").get();
-            }
-            if (userRepository.findByUsername("코스").isEmpty()) {
-                cos = userRepository.save(newUser("코스", "USER", true));
-            } else {
-                cos = userRepository.findByUsername("코스").get();
-            }
-            if (userRepository.findByUsername("러브").isEmpty()) {
-                love = userRepository.save(newUser("러브", "USER", false));
-            } else {
-                love = userRepository.findByUsername("러브").get();
-            }
+            User ssar = registerUser(userRepository, "사르", "ADMIN", true);
+            User cos = registerUser(userRepository, "코스", "USER", true);
+            User love = registerUser(userRepository, "러브", "USER", false);
 
-            // 등록되어 있지 않은 경우에만 초기화
-            if (userRepository.findByUsername("사르").isEmpty() || userRepository.findByUsername("코스").isEmpty() || userRepository.findByUsername("러브").isEmpty()) {
-                Annual annual1 = annualRepository.save(newAnnual(LocalDate.of(2023, 06, 1), LocalDate.of(2023, 06, 5), 5L));
-                Duty duty1 = dutyRepository.save(newDuty(LocalDate.of(2023, 06, 1)));
-                Annual annual2 = annualRepository.save(newAnnual(LocalDate.of(2023, 06, 7), LocalDate.of(2023, 06, 9), 3L));
-                Duty duty2 = dutyRepository.save(newDuty(LocalDate.of(2023, 06, 5)));
-                Event event1 = eventRepository.save(newEvent(cos, "ANNUAL", annual1, null));
-                Event event2 = eventRepository.save(newEvent(cos, "DUTY", null, duty1));
-                Event event3 = eventRepository.save(newEvent(cos, "ANNUAL", annual2, null));
-                Event event4 = eventRepository.save(newEvent(cos, "DUTY", null, duty2));
-                orderRepository.save(newOrder(event1, OrderState.WAITING, null));
-                orderRepository.save(newOrder(event2, OrderState.WAITING, null));
-                orderRepository.save(newOrder(event3, OrderState.APPROVED, ssar));
-                orderRepository.save(newOrder(event4, OrderState.APPROVED, ssar));
-            }
+            Annual annual1 = annualRepository.save(newAnnual(LocalDate.of(2023, 06, 1), LocalDate.of(2023, 06, 5), 5L));
+            Duty duty1 = dutyRepository.save(newDuty(LocalDate.of(2023, 06, 1)));
+            Annual annual2 = annualRepository.save(newAnnual(LocalDate.of(2023, 06, 7), LocalDate.of(2023, 06, 9), 3L));
+            Duty duty2 = dutyRepository.save(newDuty(LocalDate.of(2023, 06, 5)));
+            Event event1 = eventRepository.save(newEvent(cos, "ANNUAL", annual1, null));
+            Event event2 = eventRepository.save(newEvent(cos, "DUTY", null, duty1));
+            Event event3 = eventRepository.save(newEvent(cos, "ANNUAL", annual2, null));
+            Event event4 = eventRepository.save(newEvent(cos, "DUTY", null, duty2));
+            orderRepository.save(newOrder(event1, OrderState.WAITING, null));
+            orderRepository.save(newOrder(event2, OrderState.WAITING, null));
+            orderRepository.save(newOrder(event3, OrderState.APPROVED, ssar));
+            orderRepository.save(newOrder(event4, OrderState.APPROVED, ssar));
         };
     }
+
+    private User registerUser(UserRepository userRepository, String username, String role, boolean enabled) {
+        if (userRepository.findByUsername(username).isEmpty()) {
+            return userRepository.save(newUser(username, role, enabled));
+        }
+        return userRepository.findByUsername(username).get();
+    }
+
 
 
     @Profile("prod")
