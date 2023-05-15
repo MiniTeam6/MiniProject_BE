cf7ee781013ec3a4a4a52b11a20bed5147e464cf
ji-hoooon <ljh6384@naver.com>
2023-05-15 19:38:05 +0900

    리포지토리 어노테이션 떼기

--- src/main/java/shop/mtcoding/restend/model/log/LoginLogRepository.java
+++ src/main/java/shop/mtcoding/restend/model/log/LoginLogRepository.java
@@ -1,7 +1,7 @@
-package shop.minostreet.shoppingmall.repository;
+package shop.mtcoding.restend.model.log;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import shop.minostreet.shoppingmall.domain.LoginLog;
-
 public interface LoginLogRepository extends JpaRepository<LoginLog, Long> {
 }
