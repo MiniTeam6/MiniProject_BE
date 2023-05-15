01d2f6b19ceddce5af0f9eae9bde4b8d9fccb156
yeeeeerim <gyr3904@gmail.com>
2023-05-15 18:13:41 +0900

    에러코드 추가

--- src/main/java/shop/mtcoding/restend/core/advice/MyExceptionAdvice.java
+++ src/main/java/shop/mtcoding/restend/core/advice/MyExceptionAdvice.java
@@ -55,8 +55,7 @@ public class MyExceptionAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> unknownServerError(Exception e){
-        Sentry.captureException(e);
-        ResponseDTO<String> responseDTO = new ResponseDTO<>(HttpStatus.INTERNAL_SERVER_ERROR, "unknownServerError", e.getMessage());
+        ResponseDTO<String> responseDTO = new ResponseDTO<>(HttpStatus.INTERNAL_SERVER_ERROR, "unknownServerError", e.getMessage(),100);
         return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
